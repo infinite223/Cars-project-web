1068ad3bf493df1c9c914e267cb817e63f552cfe
infinite223 <dawidszmigiel9@gmail.com>
2023-02-12 12:24:02 +0100

    add options to project page

--- src/pages/project/optionsProject/index.tsx
+++ src/pages/project/optionsProject/index.tsx
@@ -1,6 +1,9 @@
 import './styles.scss'
 import { FC } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
+import { FaRegShareSquare } from 'react-icons/fa'
+import { BiCopy } from 'react-icons/bi'
+import { MdReportGmailerrorred } from 'react-icons/md'
 
 interface Props {
     showOptions: boolean,
@@ -10,12 +13,37 @@ interface Props {
 export const OptionsProject:FC<Props> = ({setShowOptions, showOptions}) => {
     return (
         <AnimatePresence>
-            <motion.div onClick={()=>setShowOptions(false)} className='optionsProject'  key="modal"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }} style={{display:showOptions?'flex':'none'}}>
-                dasdsa
-            </motion.div>
+            {showOptions &&
+                <motion.div 
+                    onClick={()=>setShowOptions(false)} 
+                    className='optionsProject'  
+                    key="modal"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    <motion.div
+                        key="modal"
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        exit={{ scale: 0, opacity: 0 }} 
+                        className='optionsProject__container'
+                    >
+                        <div className='optionsProject__container-option'>          
+                            <FaRegShareSquare size={18}/>
+                            <div>Udostępnij w aplikacji</div>
+                        </div>
+                        <div className='optionsProject__container-option'>
+                            <BiCopy size={20}/>
+                            <div>Kopiuj link</div>
+                        </div>
+                        <div className='optionsProject__container-option red'>
+                            <MdReportGmailerrorred size={20}/>
+                            <div>Zgłoś ten projekt</div>
+                        </div>
+                    </motion.div>
+                </motion.div>
+            }
         </AnimatePresence>
 
     )
