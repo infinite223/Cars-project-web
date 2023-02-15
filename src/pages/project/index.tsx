584931cecbdb13eb4ad28ee050f4a16626ac769d
infinite223 <dawidszmigiel9@gmail.com>
2023-02-15 23:32:54 +0100

    add map in project page and some details

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -13,10 +13,15 @@ import { CircleGradient } from './circle';
 import { useAudio } from '../../hooks/useAudio';
 import { AiFillFacebook } from 'react-icons/ai';
 import { OptionsProject } from './optionsProject';
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
+
+const libraries:("places" | "drawing" | "geometry" | "localContext" | "visualization")[]= ['places']
+
 
 export const ProjectPage = () => {
   const { logout, user }:any =  useAuth()  
   const navigate = useNavigate()
+  const [mapInstance, setMapInstance] = useState<any>()
   const [showOptions, setShowOptions] = useState(false)
   const location = useLocation();
 
@@ -29,6 +34,10 @@ export const ProjectPage = () => {
   const {car, id, place, createdAt, author} = location.state as CarprojectData;
   const { toggle, playing }:any  = useAudio(car.soundCheck)
   const colors = car.performance && getColorsCircle(car.performance[0].value, car.performance[0].type)
+
+  const { isLoaded, loadError } =  useLoadScript({googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API?process.env.REACT_APP_GOOGLE_MAPS_API:'',
+    libraries
+  })
 
   return (
     <motion.div className='projectPage' animate={{opacity:[0.6, 1]}}>
@@ -65,6 +74,25 @@ export const ProjectPage = () => {
 
             </div>
 
+            <motion.footer className='projectPage__footer'>
+                <div className='projectPage__footer-icons'>
+                  <motion.div whileHover={colors?{color: colors[0]}:{}}>
+                    <FiSend size={22} className="icon"/>
+                  </motion.div>
+                  <motion.div whileHover={colors?{color: colors[0]}:{}}>
+                    <BsFillShareFill  size={22} className="icon"/>
+                  </motion.div>
+                  <motion.div whileHover={colors?{color: colors[0]}:{}}>
+                    <FiHeart size={24} className="icon"/>
+                  </motion.div>
+                  {car.likes.length}
+                </div>
+
+                <motion.div className='projectPage__footer-author' whileHover={{color: 'white'}}>
+                  <div className='authorText'>{author.name}</div>
+                  <img src={author.imageUri} className="authorImg"/>
+                </motion.div>
+              </motion.footer>
             <motion.div className='projectPage__main_content-info'>
               <h1>Podstawowe informacjie</h1>   
               <div className='projectPage__main_content-info-contaier'>
@@ -84,24 +112,47 @@ export const ProjectPage = () => {
                             {car.history.length===0?'STOCK':'STAGE '+car.history.length}   
                 </div>}
               </div>
-              <div className='description'>{car.description}random text dsadsad</div>
+              <div className='description'>{car.description}</div>
+
+  
 
               <div className='projectPage__main_content-info-links'>
-              <div className='link' style={{color:car.links.yt?'white':'gray'}}>
+              <div className='link' style={{color:car.links.yt?'white':'rgba(50, 50, 50, .5)'}}>
                   <BsYoutube size={20}/>
                   <div className='link-type'>Youtube</div>
                 </div>
-                <div className='link' style={{color:car.links.yt?'white':'gray'}}>
+                <div className='link' style={{color:car.links.yt?'white':'rgba(50, 50, 50, .5)'}}>
                   <BsInstagram size={20}/>
                   <div className='link-type'>Instagram</div>
                 </div>
-                <div className='link' style={{color:car.links.yt?'white':'gray'}}>
+                <div className='link' style={{color:car.links.yt?'white':'rgba(50, 50, 50, .5)'}}>
                   <AiFillFacebook size={22}/>
                   <div className='link-type'>Facebook</div>
                 </div>
               </div>
             </motion.div>
+
+              {place && <div className='map'>
+                <GoogleMap
+                    zoom={10}
+                    center={{
+                      lat: place.latitude?place.latitude:0,
+                      lng: place.longitude?place.longitude:0
+                    }}
+                    mapContainerClassName='mapView'
+                    //onClick={onMapClick}
+                    onLoad={(map) =>setMapInstance(map)}
+                  >
+                      {mapInstance &&<Marker
+                          position={{
+                              lat: place.latitude?place.latitude:0,
+                              lng:place.longitude?place.longitude:0
+                          }}
+                      />}
+                </GoogleMap>
+              </div>}
           </motion.div>
+          
        
           <motion.div className='projectPage__main_history'
             style={colors?{borderColor: 
@@ -122,25 +173,6 @@ export const ProjectPage = () => {
             }
           </motion.div>
         </div>
-        <motion.footer className='projectPage__footer'>
-          <div className='projectPage__footer-icons'>
-            <motion.div whileHover={colors?{color: colors[0]}:{}}>
-              <FiSend size={22} className="icon"/>
-            </motion.div>
-            <motion.div whileHover={colors?{color: colors[0]}:{}}>
-              <BsFillShareFill  size={22} className="icon"/>
-            </motion.div>
-            <motion.div whileHover={colors?{color: colors[0]}:{}}>
-              <FiHeart size={24} className="icon"/>
-            </motion.div>
-            {car.likes.length}
-          </div>
-
-          <motion.div className='projectPage__footer-author' whileHover={{color: 'white'}}>
-            <div className='authorText'>{author.name}</div>
-            <img src={author.imageUri} className="authorImg"/>
-          </motion.div>
-        </motion.footer>
     </motion.div>
   )
 } 
